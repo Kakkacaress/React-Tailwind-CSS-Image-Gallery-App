aaa2208a27a5a833fdc443c208f892ededc7e0f4
Kaleyemijowurolo <caresskakka@yahoo.com>
2020-12-16 20:57:42 +0100

    added imageCard component and fetched images from pixabay api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-
-
+import React, { useState, useEffect } from "react";
+import { ImageCard } from "./components/imageCard";
 function App() {
-  return (
-    <div >
-      <h1 className="text-6xl"> Hello TailWind</h1>
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
 
-    </div >
-     
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits)
+      setIsLoading(false)
+    })
+    .catch(err => console.log(err))
+    ;
+  }, []); 
+
+  return (
+   <div className="container mx-auto">
+    <div className="grid grid-cols-3 gap-4">
+      {images.map((image) => (
+         <ImageCard key={image.id} image={image}/>
+      ))}
+    </div>
+   </div> 
   );
 }
 
