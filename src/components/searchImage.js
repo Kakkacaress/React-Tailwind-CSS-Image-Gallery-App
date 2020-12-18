fdb6ef89e6e59a4404f3cb43c683aa65ecdf2759
Kaleyemijowurolo <caresskakka@yahoo.com>
2020-12-18 11:02:48 +0100

    added tailwind-form

---
+++ src/components/searchImage.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const searchImage = props => {
+const [state, setState] = useState(InitialState);
+    return 
+    <div>
+
+    </div>;
+};
+
+
+export { searchImage };
