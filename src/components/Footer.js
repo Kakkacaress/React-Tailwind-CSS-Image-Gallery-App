84a8bda778974f6e9ec50537a36597f905ee5f61
Kaleyemijowurolo <caresskakka@yahoo.com>
2020-12-18 12:07:17 +0100

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = props => {
+    return <div className="w-full p-20">
+        &copy; Kayode Kaleyemi, 2020.
+    </div>;
+};
+
+
+export default Footer ;
