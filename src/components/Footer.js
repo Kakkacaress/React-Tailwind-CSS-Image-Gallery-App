6637118cb13c461d2b61a981b7a9a9601d7690b2
Kaleyemijowurolo <caresskakka@yahoo.com>
2021-04-07 18:48:54 +0100

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = props => {
     return <div className="w-full p-20 text-center">
-        &copy; Kayode Kaleyemi, 2021 | @TailwindCSS.
+        &copy; Kayode Kaleyemi, 2021 | React & TailwindCSS.
     </div>;
 };
 
